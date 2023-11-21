abc0a5a145a168d17b6739be6e6b431f4a3975c0
GabrielNito <132837450+GabrielNito@users.noreply.github.com>
2023-11-21 18:55:38 -0300

    Add files via upload

--- src/cookies/bag.js
+++ src/cookies/bag.js
@@ -10,7 +10,6 @@ function products_bag() {
         parentTag[contador].appendChild(h2)
         contador++
     }
-
     function data_product_container(title, price, path) {
         let img = document.createElement("img")
         let button = document.createElement("button")
@@ -27,7 +26,6 @@ function products_bag() {
         parentTag[contador].appendChild(button);
         data_info_container(title, price)
     }
-
     function create_product_container(title, price, path) {
         let section = document.getElementsByClassName("products")
         let div = document.createElement("div")
@@ -35,7 +33,6 @@ function products_bag() {
         section[0].appendChild(div)
         data_product_container(title, price, path)
     }
-
     for (let i = 0; i < sessionStorage.length; i++) {
         data = JSON.parse(sessionStorage.getItem(Object.keys(sessionStorage)[i]))
         nome = data.nome
@@ -43,12 +40,28 @@ function products_bag() {
         create_product_container(nome, preco, "teste")
     }
 }
-
+function update_price_bag() {
+    subtotal_text = document.querySelector(".subtotal_text")
+    total_text = document.querySelector(".total_text")
+    subtotal_price = document.querySelector(".subtotal_price")
+    total_price = document.querySelector(".total_price")
+    shipping = document.querySelector(".shipping")
+    let price = 0
+    for (let i = 0; i < sessionStorage.length; i++) {
+        data = JSON.parse(sessionStorage.getItem(Object.keys(sessionStorage)[i]))
+        preco = parseFloat(data.preco.replace(",", ".").split("$")[1])
+        price += preco
+    }
+    final_price = price + 20
+    subtotal_text.innerHTML = "Subtotal"
+    total_text.innerHTML = "Total"
+    subtotal_price.innerHTML = price
+    total_price.innerHTML = final_price
+}
 function delete_item_from_bag(callback) {
     const remove = document.getElementsByClassName("remove")
-
     for (let i = 0; i < remove.length; i++) {
-        remove[i].addEventListener('click', event => {
+        remove[i].addEventListener('click', () => {
             try {
                 product = remove[i].parentNode;
                 product.remove()
@@ -57,43 +70,12 @@ function delete_item_from_bag(callback) {
             } catch (e) {
                 alert("Não foi possível remover o item da sacola, por favor atualize a página e tente novamente")
             }
-            callback
+            callback()
         })
     }
-
 }
-
-function update_price_bag() {
-    subtotal_text = document.querySelector(".subtotal_text")
-    total_text = document.querySelector(".total_text")
-    subtotal_price = document.querySelector(".subtotal_price")
-    total_price = document.querySelector(".total_price")
-    shipping = document.querySelector(".shipping")
-
-    let price = 0
-
-    for (let i = 0; i < sessionStorage.length; i++) {
-        data = JSON.parse(sessionStorage.getItem(Object.keys(sessionStorage)[i]))
-        preco = parseFloat(data.preco.replace(",", ".").split("$")[1])
-        price += preco
-    }
-    final_price = price + 20
-    console.log(subtotal_price, total_price, subtotal_text, total_text);
-
-    // subtotal_text.innerHTML = "Subtotal"
-    // total_text.innerHTML = "Total"
-    // subtotal_price.innerHTML = price
-    // total_price.innerHTML = final_price
-    document.querySelector(".subtotal_text").innerHTML = "Subtotal"
-    // total_text.innerHTML = "Total"
-    // subtotal_price.innerHTML = price
-    // total_price.innerHTML = final_price
-}
-
 function bag_prices(callback) {
-
     const order = document.getElementsByClassName("order")
-    console.log(order);
     const h1_1 = document.createElement("h1")
     h1_1.classList.add("subtotal_text")
     const h1_2 = document.createElement("h1")
@@ -117,11 +99,15 @@ function bag_prices(callback) {
     order[0].appendChild(h2_2)
     order[0].appendChild(button)
 
-    callback
+    button.addEventListener("click", () => {
+        alert("Compra efetuada com sucesso")
+        window.location.replace("http://localhost:5173/index.html")
+    })
+    callback()
 }
-
 document.addEventListener("DOMContentLoaded", () => {
     products_bag()
-    bag_prices(update_price_bag())
-    delete_item_from_bag(update_price_bag())
+    bag_prices(update_price_bag)
+    delete_item_from_bag(update_price_bag)
 })
+
